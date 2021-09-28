e4a3882d1e60890ccb3c9b8f9623ca9d17d4df8a
Bruno <brunolamarca@outlook.com>
2021-09-27 23:10:58 -0300

    commit inicial aula 1 sniperBot

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import useWebSocket from 'react-use-websocket';
+
+function App() {
+
+  const [ticker, setTicker] = useState({});
+  const [tradingview, setTradingView] = useState({});
+
+  const {lastJsonMessage} = useWebSocket('wss://stream.binance.com:9443/stream?streams=btcusdt@ticker', {
+    onMessage: () => {
+      if (lastJsonMessage && lastJsonMessage.data) {
+        if (lastJsonMessage.stream === 'btcusdt@ticker'){
+          setTicker(lastJsonMessage.data);
+        }
+      }
+    },
+    onError: (event) => {
+      alert(event);
+    }
+  })
+
+  useEffect(() => {
+    const tv = new window.TradingView.widget(
+      {
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "60",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "br",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "details": true,
+        "container_id": "tradingview_0b2f8"
+      }
+    );
+    setTradingView(tv);
+  }, [])
+
+  return (
+    <div>
+      <h1>SniperBot 1.0</h1>
+      <div class="tradingview-widget-container">
+        <div id="tradingview_0b2f8"></div>
+      </div>
+      <div>
+        <b>Ticker 24h:</b> <br/>
+        Open: {ticker && ticker.o}<br/>
+        High: {ticker && ticker.h}<br/>
+        Low: {ticker && ticker.l}<br/>
+        Last: {ticker && ticker.c}<br/>
+        Change %: {ticker && ticker.P}<br/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
